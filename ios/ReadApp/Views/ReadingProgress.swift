5eeb2065ec471a9f612732e51736446117c42fcd
hizhongcai <hizhongcai@gmail.com>
2026-01-06 10:12:37 -0500

    Fix saveReadingProgress call signature

--- ios/ReadApp/Views/ReadingProgress.swift
+++ ios/ReadApp/Views/ReadingProgress.swift
@@ -10,7 +10,7 @@ extension ReadingView {
         Task {
             let title = chapters.indices.contains(chapterIdx) ? chapters[chapterIdx].title : nil
             // 保存到本地
-            preferences.saveReadingProgress(bookUrl: bookUrl, chapterIndex: chapterIdx, pageIndex: 0, bodyCharIndex: Int(pos * 10000), timestamp: Int(Date().timeIntervalSince1970 * 1000))
+            preferences.saveReadingProgress(bookUrl: bookUrl, chapterIndex: chapterIdx, pageIndex: 0, bodyCharIndex: Int(pos * 10000))
             // 保存到服务器
             try? await apiService.saveBookProgress(bookUrl: bookUrl, index: chapterIdx, pos: pos, title: title)
         }
