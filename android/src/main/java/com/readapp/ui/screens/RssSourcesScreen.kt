2de0b2057dc653d16e194370f547954932deb0d9
hizhongcai <hizhongcai@gmail.com>
2026-01-09 07:52:45 -0500

    Fix iOS project references and opt-in Material3

--- android/src/main/java/com/readapp/ui/screens/RssSourcesScreen.kt
+++ android/src/main/java/com/readapp/ui/screens/RssSourcesScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -37,6 +38,7 @@ import com.readapp.ui.theme.AppDimens
 import com.readapp.viewmodel.BookViewModel
 import com.readapp.viewmodel.RssViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RssSourcesScreen(
     bookViewModel: BookViewModel,
